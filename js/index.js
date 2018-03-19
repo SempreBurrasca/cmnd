104a81e7d8e840f4905c95ed7719e1c03e0bc65b
Gian Lorenzo <gian.comandini@gmail.com>
2018-03-19 11:58:53 +0100

    Added scrollspy

--- js/index.js
+++ js/index.js
@@ -2,3 +2,15 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
   });
 $('.carousel.carousel-slider').carousel({fullWidth: true});
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy({scrollOffset:0});
+  });
+  $( "#ios" ).hover(function() {
+    $( this ).toggleClass( "indigo " );
+  });
+  $( "#sono" ).hover(function() {
+    $( this ).toggleClass( "blue " );
+  });
+  $( "#semp" ).hover(function() {
+    $( this ).toggleClass( "teal " );
+  });
