721181dc36b82b29b51892fd962d627e595c4482
Gian Lorenzo <gian.comandini@gmail.com>
2018-03-19 12:04:01 +0100

    fixed bg scroll

--- js/index.js
+++ js/index.js
@@ -7,10 +7,13 @@ $(document).ready(function(){
   });
   $( "#ios" ).hover(function() {
     $( "#her" ).toggleClass( "indigo " );
+    $("#her").removeClass("light-green darken-1")
   });
   $( "#sono" ).hover(function() {
     $( "#her" ).toggleClass( "blue " );
+    $("#her").removeClass("light-green darken-1")
   });
   $( "#semp" ).hover(function() {
     $( "#her" ).toggleClass( "teal " );
+    $("#her").removeClass("light-green darken-1")
   });
